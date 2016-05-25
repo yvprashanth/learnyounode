84b2248b967d5b4c6af2715629351379640e6925
Prashanth Yerramilli <yvprashanth@gmail.com>
2016-05-24 18:34:42 -0700

    some more

--- filteredlist.js
+++ filteredlist.js
@@ -14,15 +14,34 @@ fs.readdir(folder, function(err, files){
 });
 
 
-module.exports =  function(path, extension, cb) {
-  var regex = new RegExp(".*\\." + escape(extension) + "$")
-	fs.readdir(path, function(err, filenames) {
-		if (err) {
-			cb(err)
-		} else {
-			cb(false, filenames.filter(function(file) {
-				return regex.test(file)
-			}))
-		}
-	})
-}
+module.exports = function(dirname,extension,callback){
+    var list = [];
+    var i = 0;
+
+    fs.readdir(dirname, function(err,files){
+        if(err){
+            return callback(err);
+        }
+        else{
+            files.forEach(function(fileName){
+                if(path.extname(fileName) === extension){
+                    list.push(fileName);
+                }
+            })
+        }
+        return callback(null,list);
+    })
+};
+
+// module.exports =  function(path, extension, cb) {
+//   var regex = new RegExp(".*\\." + escape(extension) + "$")
+// 	fs.readdir(path, function(err, filenames) {
+// 		if (err) {
+// 			cb(err)
+// 		} else {
+// 			cb(false, filenames.filter(function(file) {
+// 				return regex.test(file)
+// 			}))
+// 		}
+// 	})
+// }
